3d7f3bc012f08641c4985c8f3130ef0e6bf5efcd
Zurie Mai <pmai6@gatech.edu>
2017-10-15 06:27:19 -0400

    add logo

--- assets/js/healthmap.js
+++ assets/js/healthmap.js
@@ -1,4 +1,4 @@
-var svg = d3.select("U.S."),
+var svg = d3.select("svg"),
     width = +svg.attr("width"),
     height = +svg.attr("height");
 
@@ -38,11 +38,11 @@ g.append("text")
     .attr("fill", "#000")
     .attr("text-anchor", "start")
     .attr("font-weight", "bold")
-    .text("Unemployment rate");
+    .text("Medical Equipment need");
 
 g.call(d3.axisBottom(x)
     .tickSize(13)
-    .tickFormat(function(x, i) { return i ? x : x + "%"; })
+    .tickFormat(function(x, i) { return i ? x*10 : x*10; })
     .tickValues(color.domain()))
   .select(".domain")
     .remove();
@@ -63,7 +63,7 @@ function ready(error, us) {
       .attr("fill", function(d) { return color(d.rate = unemployment.get(d.id)); })
       .attr("d", path)
     .append("title")
-      .text(function(d) { return d.rate + "%"; });
+      .text(function(d) { return "Estimated donation: " + d.rate*10; });
 
   svg.append("path")
       .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
