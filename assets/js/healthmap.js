1f32067af58825541e8b0292112bd50624a84c17
Zurie Mai <pmai6@gatech.edu>
2017-10-15 07:04:34 -0400

    map works again

--- assets/js/healthmap.js
+++ assets/js/healthmap.js
@@ -1,72 +1,73 @@
-var svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
 
-var unemployment = d3.map();
+      var svg = d3.select("svg"),
+              width = +svg.attr("width"),
+              height = +svg.attr("height");
 
-var path = d3.geoPath();
+          var unemployment = d3.map();
 
-var x = d3.scaleLinear()
-    .domain([1, 10])
-    .rangeRound([600, 860]);
+          var path = d3.geoPath();
 
-var color = d3.scaleThreshold()
-    .domain(d3.range(2, 10))
-    .range(d3.schemeBlues[9]);
+          var x = d3.scaleLinear()
+              .domain([1, 10])
+              .rangeRound([600, 860]);
 
-var g = svg.append("g")
-    .attr("class", "key")
-    .attr("transform", "translate(0,40)");
+          var color = d3.scaleThreshold()
+              .domain(d3.range(2, 10))
+              .range(d3.schemeBlues[9]);
 
-g.selectAll("rect")
-  .data(color.range().map(function(d) {
-      d = color.invertExtent(d);
-      if (d[0] == null) d[0] = x.domain()[0];
-      if (d[1] == null) d[1] = x.domain()[1];
-      return d;
-    }))
-  .enter().append("rect")
-    .attr("height", 8)
-    .attr("x", function(d) { return x(d[0]); })
-    .attr("width", function(d) { return x(d[1]) - x(d[0]); })
-    .attr("fill", function(d) { return color(d[0]); });
+          var g = svg.append("g")
+              .attr("class", "key")
+              .attr("transform", "translate(0,40)");
 
-g.append("text")
-    .attr("class", "caption")
-    .attr("x", x.range()[0])
-    .attr("y", -6)
-    .attr("fill", "#000")
-    .attr("text-anchor", "start")
-    .attr("font-weight", "bold")
-    .text("Medical Equipment need");
+          g.selectAll("rect")
+            .data(color.range().map(function(d) {
+                d = color.invertExtent(d);
+                if (d[0] == null) d[0] = x.domain()[0];
+                if (d[1] == null) d[1] = x.domain()[1];
+                return d;
+              }))
+            .enter().append("rect")
+              .attr("height", 8)
+              .attr("x", function(d) { return x(d[0]); })
+              .attr("width", function(d) { return x(d[1]) - x(d[0]); })
+              .attr("fill", function(d) { return color(d[0]); });
 
-g.call(d3.axisBottom(x)
-    .tickSize(13)
-    .tickFormat(function(x, i) { return i ? x*10 : x*10; })
-    .tickValues(color.domain()))
-  .select(".domain")
-    .remove();
+          g.append("text")
+              .attr("class", "caption")
+              .attr("x", x.range()[0])
+              .attr("y", -6)
+              .attr("fill", "#000")
+              .attr("text-anchor", "start")
+              .attr("font-weight", "bold")
+              .text("Equipment Donation needed");
 
-d3.queue()
-    .defer(d3.json, "https://d3js.org/us-10m.v1.json")
-    .defer(d3.tsv, "data/unemployment.tsv", function(d) { unemployment.set(d.id, +d.rate); })
-    .await(ready);
+          g.call(d3.axisBottom(x)
+              .tickSize(13)
+              .tickFormat(function(x, i) { return i ? x*10 : x*10; })
+              .tickValues(color.domain()))
+            .select(".domain")
+              .remove();
 
-function ready(error, us) {
-  if (error) throw error;
+          d3.queue()
+              .defer(d3.json, "https://d3js.org/us-10m.v1.json")
+              .defer(d3.tsv, "assets/data/unemployment.tsv", function(d) { unemployment.set(d.id, +d.rate); })
+              .await(ready);
 
-  svg.append("g")
-      .attr("class", "counties")
-    .selectAll("path")
-    .data(topojson.feature(us, us.objects.counties).features)
-    .enter().append("path")
-      .attr("fill", function(d) { return color(d.rate = unemployment.get(d.id)); })
-      .attr("d", path)
-    .append("title")
-      .text(function(d) { return "Estimated donation: " + d.rate*10; });
+          function ready(error, us) {
+            if (error) throw error;
 
-  svg.append("path")
-      .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-      .attr("class", "states")
-      .attr("d", path);
-}
+            svg.append("g")
+                .attr("class", "counties")
+              .selectAll("path")
+              .data(topojson.feature(us, us.objects.counties).features)
+              .enter().append("path")
+                .attr("fill", function(d) { return color(d.rate = unemployment.get(d.id)); })
+                .attr("d", path)
+              .append("title")
+                .text(function(d) { return d.rate; });
+
+            svg.append("path")
+                .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+                .attr("class", "states")
+                .attr("d", path);
+          }
